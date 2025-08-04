173b0a168f22a804cb0eb36b9738087599d70bfd
UselessHumster <79360700+UselessHumster@users.noreply.github.com>
2025-08-04 15:09:51 +0300

    Added format documentation

--- gendiff/documentation/arguments.py
+++ gendiff/documentation/arguments.py
@@ -3,7 +3,8 @@ import argparse
 
 def create_parser():
     parser = argparse.ArgumentParser(prog='gendiff')
-    parser.add_argument('-f', '--format', help='set format of output')
+    parser.add_argument('-f', '--format', help='set format of output',
+                        choices=['stylish', 'plain', 'json'])
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     return parser
